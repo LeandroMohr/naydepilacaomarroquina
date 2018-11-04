3447de38d8031eef6b59cb0395f7a9a8b7eca70a
Leandro Marcelo Mohr <marceloleandro_2@hotmail.com>
2018-11-04 21:44:22 -0200

    Ajustado html e incluido foto do formulario e a foto do depoimento

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,33 +1,16 @@
 $('.depoimento').slick({
 	infinite: true,
-	centerMode: true,
-	slidesToShow: 3,
-	slidesToScroll: 3,
+	centerMode: false,
+	arrows: false,
+	dots: false,
+	mobileFirst: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
 	responsive: [
 		{
-			breakpoint: 2560,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 3
-			}
-		},
-		{
 			breakpoint: 1024,
-			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2
-			}
-		},
-		{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
-			}
+			settings: "unslick"
 		}
-		// You can unslick at a given breakpoint now by adding:
-		// settings: "unslick"
-		// instead of a settings object
 	]
 });
 
